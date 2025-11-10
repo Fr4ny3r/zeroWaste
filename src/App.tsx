c16ed159089993b9a8e35426511ff705d943cc4e
Fr4ny3r <guevarafranyer09@gmail.com>
2025-11-10 10:48:31 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+import Hero from './components/Hero'
+import './App.css'
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+
+ 
+
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+
+
+function Hero () {
+    return (
+        <header className="bg-[var(--color-primary)] flex justify-around items-center relative left-0 w-full h-[750px]">
+            <img src="/logo.png" alt="Logo" className="w-48 mx-auto mb-6 left-15 top-0 absolute" />
+            <div className="text-center p-8 bg-[var(--color-primary)] bg-opacity-70">
+
+            </div>
+            <div></div>
+        </header>
+    )
+}
+
+export default Hero
