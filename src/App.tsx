4e5e6ab47da9535b9da6663f87b5deccce1d8786
Fr4ny3r <guevarafranyer09@gmail.com>
2025-11-10 11:13:22 -0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 // import React, { useState, useEffect } from 'react'
 import Hero from './components/Hero'
 import './App.css'
-// import { gsap } from "gsap";
-// import { useGSAP } from "@gsap/react";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
 
- 
+
+    useGSAP(() => {
+      const tl = gsap.timeline();
+
+      tl.fromTo(
+        ".product",
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          stagger: 0.3,
+          ease: "power2.out",
+        }
+      );
+
+      return tl;
+    });
+
+
 
   return (
     <>
